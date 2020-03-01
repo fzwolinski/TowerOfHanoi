1d9a17f2799f8a6f2ea4e0ee92b08fc8cf3944a2
Fabian Zwolinski <Giovacho44@gmail.com>
2020-03-01 23:02:15 +0100

    Added Create-Map functionality. Added draw_map()

--- main.cpp
+++ main.cpp
@@ -1,31 +1,22 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 
-void create_map(int num_of_disks);
 void check_args(int arg_count, char *a[]);
+void create_map(int disks_count);
+void draw_map();
 
 int num_of_disks = 3;
+int arr[3][7];
 
 int main(int argc, char** argv)
 {
     check_args(argc, argv);
-
-    int arr[3][3] = {
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0;j < 3; j++) {
-            std::cout << arr[j][i] << "\t";
-        }
-        std::cout << std::endl;
-    }
+    create_map(num_of_disks);
+    draw_map();
 
     return 0;
 }
-
 
 void check_args(int arg_count, char *a[]) {
     int arg_content;
@@ -36,3 +27,44 @@ void check_args(int arg_count, char *a[]) {
         }
     }
 }
+
+void create_map(int disks_count) {
+    int map_val = 2; // the number of blocks for one disk must be even to make the whole structure symmetrical
+    /*
+   ##
+  ####
+ ######
+########
+
+    {2, 4, 6, 8, 10, 0 , 0},
+    {0, 0, 0, 8, 10, 0 , 0},
+    {0, 4, 6, 8, 10, 0 , 0},
+
+    */
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < disks_count;j++) {
+            arr[i][j] = i == 0 ? map_val : 0;
+            map_val += 2;
+        }
+    }
+}
+
+void draw_map() {
+    int how_many_spaces = num_of_disks;
+    for(int i = 0; i < num_of_disks; i++) {
+        for(int j = 0;j < 3; j++) {
+            std::cout << std::string(how_many_spaces, ' ');
+            for(int b = 0; b < arr[j][i];b++)
+                std::cout << "#";
+            if (arr[j][i] == 0)     // In case there is no disk at this place
+                std::cout << "\t";
+            std::cout << "\t";
+        }
+        how_many_spaces = how_many_spaces != 0 ? how_many_spaces-1 : 0;
+        std::cout << std::endl;
+    }
+    //std::cout << "-----";
+
+}
+
+
